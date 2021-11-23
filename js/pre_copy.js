efdd35e57f82c54c5b226edf15b4057fe9067066
Kristian Aune <kraune@verizonmedia.com>
2021-11-23 16:02:48 +0100

    Add clipboard copy button to pre elements

---
+++ js/pre_copy.js
@@ -0,0 +1,29 @@
+
+function copyPreContent(button) {
+    // Assume a pre after the copy button
+    let elem = button;
+    while (elem = elem.nextSibling) {
+        if ( elem.nodeType !== Node.TEXT_NODE) {
+            if (elem.nodeName === "PRE") {
+                copyToClipboard(removePrompt(elem.innerText));
+            }
+        }
+    }
+}
+
+function removePrompt (text) {
+    return text.replace(/^\$\s*/gm, "");
+}
+
+function copyToClipboard(text) {
+    // thanks to https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+    if (!navigator.clipboard) {
+        alert("Copy button not supported in this browser version")
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
