8e188f11e87f56d6398b626257d7a2ee667b4e9d
Kristian Aune <kraune@verizonmedia.com>
2022-01-05 12:49:22 +0100

    Set MathJax config, then load script. Smaller equations

---
+++ js/load-mathjax.js
@@ -0,0 +1,19 @@
+
+window.MathJax = {
+    //chtml: {
+    //    scale: 0.75,
+    //    minScale: .5
+    //},
+    svg: {  // http://docs.mathjax.org/en/latest/options/output/svg.html
+        scale: 0.75,
+        minScale: .5
+    }
+};
+
+(function () {
+    let script = document.createElement('script');
+    //script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js';
+    script.async = true;
+    document.head.appendChild(script);
+})();
