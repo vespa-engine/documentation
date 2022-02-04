fb9629639d5aecbf1c585fdbbf0d311ea5db4ad9
Kristian Aune <kraune@verizonmedia.com>
2022-02-04 07:52:45 +0100

    Make query more readable

--- js/search.js
+++ js/search.js
@@ -48,7 +48,9 @@ const handleLocationQuery = () => {
     document.getElementById("searchinput").value = query;
     result.innerHTML = `Searching for '${query}' ...`;
     fetch(
-      `https://doc-search.vespa.oath.cloud/search/?yql=select%20*%20from%20doc%20where%20%5B%7B%22grammar%22%3A%20%22weakAnd%22%7D%5DuserInput(@input)%3B&hits=25&ranking=documentation&locale=en-US&input=${escape(query)}`
+        encodeURI("https://doc-search.vespa.oath.cloud/search/?yql=" +
+            "select * from doc where [{\"grammar\": \"weakAnd\"}]userInput(@input)" +
+            "&hits=25&ranking=documentation&locale=en-US&input=" + query)
     )
           .then((res) => res.json())
           .then((res) => handleResults(res.root.children))
